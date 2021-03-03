34539b7d178dfff96cca2324bb3cccd533148f02
Adrian <nairda9977@gmail.com>
2021-03-03 16:35:05 +0200

    added HomePage, ProductPage

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/laba/HomePage.java
@@ -0,0 +1,28 @@
+package com.qaprosoft.carina.demo.gui.pages.laba;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends AbstractPage {
+
+    public static final Logger LOGGER = Logger.getLogger(HomePage.class);
+
+    @FindBy(id="fat-menu")
+    private ExtendedWebElement menuButton;
+
+    @FindBy(xpath = "//*[@class=\"menu-categories__link\"]")
+    private ExtendedWebElement product;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    private void selectProduct (ExtendedWebElement productOnPage) {
+        productOnPage.click();
+    }
+
+
+}
---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/laba/ProductPage.java
@@ -0,0 +1,17 @@
+package com.qaprosoft.carina.demo.gui.pages.laba;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPage extends AbstractPage {
+    public static final Logger LOGGER = Logger.getLogger(ProductPage.class);
+
+    private ExtendedWebElement
+
+
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
+}
